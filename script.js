d129f7e0cbaa8784c183aa55948b00f84badc8f3
LE Palmer <lpalmer@ucsd.edu>
2023-11-06 14:12:47 -0800

    created functions for event listener for save buttons and previously saved data from local storage to display on screen

--- script.js
+++ script.js
@@ -3,4 +3,22 @@ $(function () {
     var rightNow = dayjs().format('dddd D MMMM, YYYY');
     $("#currentDay").text(rightNow);
 
+/* add event listener to save button*/
+    $(".saveBtn").on("click", function () {
+        var hour = $(this).parent().attr("id")
+        var text = $(this).siblings(".description").val()
+        console.log(hour, text)
+        localStorage.setItem(hour, text)
+
+    })
+/* Retrieve previously saved data from local Storage to display on the screen*/
+    $("hour-9 .description").val(localStorage.getItem("hour-9"))
+    $("hour-10 .description").val(localStorage.getItem("hour-10"))
+    $("hour-11 .description").val(localStorage.getItem("hour-11"))
+    $("hour-12 .description").val(localStorage.getItem("hour-12"))
+    $("hour-13 .description").val(localStorage.getItem("hour-13"))
+    $("hour-14 .description").val(localStorage.getItem("hour-14"))
+    $("hour-15 .description").val(localStorage.getItem("hour-15"))
+    $("hour-16 .description").val(localStorage.getItem("hour-16"))
+    $("hour-17 .description").val(localStorage.getItem("hour-17"))
 });

1f2586e370b899160ccccf2149df25c9056ead36
LE Palmer <lpalmer@ucsd.edu>
2023-11-07 09:44:35 -0800

    debugged local Storage issue

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(function () {
     var rightNow = dayjs().format('dddd D MMMM, YYYY');
     $("#currentDay").text(rightNow);
 
-/* add event listener to save button*/
+/* Add event listener to save buttons */
     $(".saveBtn").on("click", function () {
         var hour = $(this).parent().attr("id")
         var text = $(this).siblings(".description").val()
@@ -13,25 +13,25 @@ $(function () {
         localStorage.setItem(hour, text)
 
     })
-/* Retrieve previously saved data from local Storage to display on the screen*/
-    $("hour-9 .description").val(localStorage.getItem("hour-9"))
-    $("hour-10 .description").val(localStorage.getItem("hour-10"))
-    $("hour-11 .description").val(localStorage.getItem("hour-11"))
-    $("hour-12 .description").val(localStorage.getItem("hour-12"))
-    $("hour-13 .description").val(localStorage.getItem("hour-13"))
-    $("hour-14 .description").val(localStorage.getItem("hour-14"))
-    $("hour-15 .description").val(localStorage.getItem("hour-15"))
-    $("hour-16 .description").val(localStorage.getItem("hour-16"))
-    $("hour-17 .description").val(localStorage.getItem("hour-17"))
+/* Retrieve previously saved data from local Storage to display on the screen */
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"))
 
-/*Dynamically style time-blocks based on current time*/    
+/* Dynamically style time-blocks based on current time */    
 $('.time-block').each(function () {
     var timeBlockHour = parseInt($(this).attr("id").split("-")[1])
 
     if (timeBlockHour < currentHour) {
         $(this).addClass('past');
 
-    } else if (timeBlockHour ===currentHour) {
+    } else if (timeBlockHour === currentHour) {
         $(this).removeClass('past');
         $(this).addClass('present');
 
@@ -40,5 +40,5 @@ $('.time-block').each(function () {
         $(this).removeClass('present');
         $(this).addClass('future')
     }
-})
+});
 })
